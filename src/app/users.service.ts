208e5132759bb65b178b024ae0e13e621e045bd7
developer-kp <arunkumarp.in@hotmail.com>
2021-04-27 11:18:56 +0200

    serive for fetching users

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../environments/environment';
+import { User } from './state/app.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+    constructor(private http: HttpClient) { }
+
+    getUsers(): Observable<User[]> {
+        return this.http.get<User[]>(environment.api.get_users);
+    }
+
 }
