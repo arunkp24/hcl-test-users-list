5485ae78b72e94d88622cf45d1df285e16257094
developer-kp <arunkumarp.in@hotmail.com>
2021-04-27 11:25:36 +0200

    user component

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,39 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { FilterOption } from './filter-option.interface';
+import { AppFacade } from './../state/app.facade';
+import { Observable } from 'rxjs';
+import { User } from '../state/app.interface';
 
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+    selector: 'app-users',
+    templateUrl: './users.component.html',
+    styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
 
-  options: FilterOption[] = [
-    {
-      value: 'name',
-      text: 'Name'
-    },
-    {
-      value: 'username',
-      text: 'User Name'
-    },
-    {
-      value: 'email',
-      text: 'Email'
-    },
-    {
-      value: 'phone',
-      text: 'Phone'
-    },
-    {
-      value: 'website',
-      text: 'Website'
+    options: FilterOption[] = [
+        {
+            value: 'name',
+            text: 'Name'
+        },
+        {
+            value: 'username',
+            text: 'User Name'
+        },
+        {
+            value: 'email',
+            text: 'Email'
+        },
+        {
+            value: 'phone',
+            text: 'Phone'
+        },
+        {
+            value: 'website',
+            text: 'Website'
+        }
+    ];
+
+    constructor(private facade: AppFacade, private formBuilder: FormBuilder) { }
+
+    // Facade for getting the users.
+    users$: Observable<User[]> = this.facade.users$;
+    // Facade for getting the error state
+    error$: Observable<Error> = this.facade.error$;
+    // Reactive form 
+    filterForm: FormGroup;
+    // Observable for the reactive form
+    filterForm$: Observable<FormGroup>;
+
+    ngOnInit(): void {
+        // Build form for HTML
+        this.filterForm = this.formBuilder.group({
+            searchColumn: [this.options[0].value],
+            searchKey: ['']
+        });
+        // Onservable for the form
+        this.filterForm$ = this.filterForm.valueChanges;
+        // Facade to load the users
+        this.facade.loadUsers();
     }
-  ];
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
